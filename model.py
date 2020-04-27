2509bad64adcbad7bf6dfe8833fe5e14669aef0c
haogerhuang <godhuang12345678@gmail.com>
2020-04-27 18:52:20 +0800

    commit

---
+++ model.py
@@ -0,0 +1,212 @@
+import torch, torch.nn as nn
+import numpy as np
+import sparseconvnet as scn
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn.functional as F
+from loss import *
+#import data
+import data_val as data
+from util import *
+
+m = 32
+residual_blocks= True
+block_reps = 2
+
+
+class SCN_sem(nn.Module):
+	def __init__(self):
+		nn.Module.__init__(self)
+		self.sparseModel = scn.Sequential().add(
+			scn.InputLayer(data.dimension,data.full_scale, mode=4)).add(
+			scn.SubmanifoldConvolution(data.dimension, 3, m, 3, False)).add(
+			scn.UNet(data.dimension, block_reps, [m, 2*m, 3*m, 4*m, 5*m, 6*m, 7*m], residual_blocks)).add(
+			scn.BatchNormReLU(m)).add(
+			scn.OutputLayer(data.dimension))
+		self.linear = nn.Linear(m, 20)
+	def forward(self,x):
+		fv=self.sparseModel(x)
+		
+		y=self.linear(fv)
+		#fv = F.normalize(fv, p=2, dim=1)
+		return y
+
+class SCN(nn.Module):
+	def __init__(self):
+		nn.Module.__init__(self)
+		self.sparseModel = scn.Sequential().add(
+			scn.InputLayer(data.dimension,data.full_scale, mode=4)).add(
+			scn.SubmanifoldConvolution(data.dimension, 3, m, 3, False)).add(
+			scn.UNet(data.dimension, block_reps, [m, 2*m, 3*m, 4*m, 5*m, 6*m, 7*m], residual_blocks)).add(
+			scn.BatchNormReLU(m)).add(
+			scn.SubmanifoldConvolution(data.dimension, m, 4, 1, False)).add(
+			scn.OutputLayer(data.dimension))
+	def forward(self,x):
+		fv=self.sparseModel(x)
+		
+		#y=self.linear(fv)
+		#fv = F.normalize(fv, p=2, dim=1)
+		return fv
+
+class RefNet(nn.Module):
+	def __init__(self):
+		nn.Module.__init__(self)
+		self.gru = nn.GRU(input_size=300, hidden_size=256, batch_first=True)
+		#self.feat_fuse = nn.Sequential(nn.Linear(36, 32), nn.ReLU())
+		self.feat_fuse = nn.Sequential(nn.Linear(128+3, 128), nn.ReLU())
+		#self.lang_sqz = nn.Sequential(nn.Linear(256, 32), nn.ReLU(), nn.Linear(32, 32))
+		self.lang_sqz = nn.Sequential(nn.Linear(256, 64), nn.ReLU(), nn.Linear(64, 64), nn.ReLU())
+		
+		self.lang_cls = nn.Sequential(nn.Linear(128, 18), nn.Dropout())
+		self.spvox_cls = nn.Sequential(nn.Linear(64, 20), nn.Dropout())
+		#self.heat_map = nn.Sequential(nn.Linear(32, 2), nn.Dropout())
+		self.heat_map = nn.Sequential(nn.Linear(128, 2), nn.Dropout())
+	def forward(self, batch):
+		#fv= self.backbone_model(batch['x'])
+		#fv = self.coord_fuse(torch.cat([fv, batch['coords'].float()], 1))	
+		total_loss = {}
+		total_loss['var'] = 0
+		total_loss['dis'] = [0,0]
+		total_loss['reg'] = 0
+		total_loss['obj'] = 0
+		idx = 0
+		for i, num_p in enumerate(batch['num_points']):
+			lang_feat = pack_padded_sequence(batch['lang_feat'][i].float().cuda(), batch['lang_len'][i], batch_first=True, enforce_sorted=False)
+			_, lang_feat = self.gru(lang_feat)
+			# Num_sentences * 128
+			lang_feat = self.lang_sqz(lang_feat.squeeze(0))
+			#lang_feat = self.lang_sqz(lang_feat[-1])
+			#loss = InsSemLoss(fv[idx:idx+num_p], batch['y_ins'][idx:idx+num_p].cuda(), batch['y'][idx:idx+num_p].cuda(), None, 0.1, 1.5, 1.5)
+			# obj_num, d  / obj_num
+			#obj_feat, obj_id = data['obj_feat']
+
+			#each_feat = fv[idx:idx+num_p]
+			# point -> supervoxel idx  , supervoxel -> instance label idx
+			supvox_ins, supvox_sem = batch['supvox'][1][i]
+			# num_supervoxel, 128
+			#supvox_feat, insupvox_var = gather_supervox(each_feat, supvox)
+
+			loss = InsSemLoss(batch['supvox_f'][i], supvox_ins.cuda(), supvox_sem.cuda(), None, 0.1, 1.5, 1.5)
+
+			supvox_feat = batch['supvox_f'][i]
+			supvox_coords = batch['supvox_coords'][i]
+			supvox_feat = torch.cat([supvox_feat, supvox_coords], -1)
+
+
+			# batch_size * 32	   (batch_size, obj_num) * (obj_num, d)
+			#obj_feat = torch.matmul((batch['lang_objID'][i].cuda().unsqueeze(-1)==obj_id).float(), obj_feat)
+			# Num_sentences, obj_num, d
+			#lang_feat_expand = lang_feat.unsqueeze(1).repeat(1, supvox_feat.shape[0], 1)
+			# Num_sentences, obj_num, d
+			#supvox_feat_expand = supvox_feat.unsqueeze(0).repeat(lang_feat.shape[0], 1, 1)
+
+			# Num_supervoxels, Num_sentences, d
+			lang_feat_expand = lang_feat.unsqueeze(0).repeat(supvox_feat.shape[0], 1, 1)
+			# Num_supervoxels, Num_sentences, d
+			supvox_feat_expand = supvox_feat.unsqueeze(1).repeat(1, lang_feat.shape[0], 1)
+
+			overall_feat = self.feat_fuse(torch.cat([supvox_feat_expand, lang_feat_expand], -1))
+			# Num_supervoxels, Num_sentences, 2
+			obj_score = self.heat_map(overall_feat)
+			# gt (Num_sentences, obj_num)
+			#obj_gt = (batch['lang_objID'][i].unsqueeze(-1).cuda() == obj_id).long()
+
+			# Num-supervoxels, Num_sentences
+			obj_gt = batch['ref_lbl'][i].cuda()
+			total_loss['obj'] += torch.nn.functional.cross_entropy(obj_score.view(-1, 2), obj_gt.view(-1), torch.Tensor([0.1, 1]).cuda())
+			total_loss['var'] += loss['var']
+			total_loss['dis'][0] += loss['dist'][0]
+			total_loss['dis'][1] += loss['dist'][1]
+			total_loss['reg'] += loss['reg']
+
+		return total_loss
+
+def gather_supervox(feat, supvox, get_var=False):
+
+	# Input: 
+	# feat: N, d /  supvox: N, 2
+	# Output: results_f: [(N1,d), (N2, d) ... (NB, d)]
+	gather_func = scn.InputLayer(1, supvox[:,0].max()+1, mode=4)
+	supvox_f = gather_func([supvox, feat])
+	supvox_idx = supvox_f.get_spatial_locations()[:,0]
+	#supvox_idx, sorted_idx = supvox_idx.sort()
+	supvox_batch = supvox_f.get_spatial_locations()[:,1]
+	results_f = []
+	results_grp = []
+	Vars = []
+	supvox_f = supvox_f.features
+	for i in range(data.batch_size):
+		each_supvox_idx = supvox_idx[supvox_batch == i]
+		each_supvox_f = supvox_f[supvox_batch==i]
+		sort_i = each_supvox_idx.argsort()
+		each_supvox_f = each_supvox_f[sort_i]
+		each_supvox_idx = each_supvox_idx[sort_i]
+		results_f.append(each_supvox_f)
+		results_grp.append(each_supvox_idx)
+		if get_var:
+			mask = supvox[:,1]==i
+			each_feat = feat[mask]
+			var = torch.norm(each_feat - each_supvox_f[supvox[:,0][mask]], p=2, dim=1)
+			Vars.append(var)
+	if get_var:
+		Vars = torch.cat(Vars, 0)
+		Vars = gather_func([supvox, (Vars**2).unsqueeze(-1)])
+		Vars = Vars.features.mean()
+		
+	return results_f, Vars
+
+
+class RefNet_Val(nn.Module):
+	def __init__(self):
+		nn.Module.__init__(self)
+		self.gru = nn.GRU(input_size=300, hidden_size=256, batch_first=True)
+		#self.feat_fuse = nn.Sequential(nn.Linear(36, 32), nn.ReLU())
+		self.feat_fuse = nn.Sequential(nn.Linear(128+3, 128), nn.ReLU())
+		self.lang_sqz = nn.Sequential(nn.Linear(256, 64), nn.ReLU(), nn.Linear(64, 64))
+		#self.lang_sqz = nn.Sequential(nn.Linear(256, 128), nn.ReLU(), nn.Linear(128, 128), nn.ReLU())
+		
+		self.lang_cls = nn.Sequential(nn.Linear(128, 18), nn.Dropout())
+		self.spvox_cls = nn.Sequential(nn.Linear(64, 20), nn.Dropout())
+		#self.heat_map = nn.Sequential(nn.Linear(32, 2), nn.Dropout())
+		self.heat_map = nn.Sequential(nn.Linear(128, 2), nn.Dropout())
+	def forward(self, batch):
+		results = []
+		ious = []
+		idx = 0
+		# Batch_num_points, supervoxel idx
+		supervox = batch['supervoxels']
+		for i, num_p in enumerate(batch['num_points']):
+			lang_feat = pack_padded_sequence(batch['lang_feat'][i].float().cuda(), batch['lang_len'][i], batch_first=True, enforce_sorted=False)
+			_, lang_feat = self.gru(lang_feat)
+			# Num_sentences * 128
+			lang_feat = self.lang_sqz(lang_feat.squeeze(0))
+			# Num_supvoxels, d
+			supvox_feat = batch['supvox_f'][i]
+			
+			supvox_coords = batch['supvox_coords'][i]
+			supvox_feat = torch.cat([supvox_feat, supvox_coords], -1)
+
+			# Num_supervoxels, Num_sentences, d
+			lang_feat_expand = lang_feat.unsqueeze(0).repeat(supvox_feat.shape[0], 1, 1)
+			# Num_supervoxels, Num_sentences, d
+			supvox_feat_expand = supvox_feat.unsqueeze(1).repeat(1, lang_feat.shape[0], 1)
+
+			overall_feat = self.feat_fuse(torch.cat([supvox_feat_expand, lang_feat_expand], -1))
+			# Num_supervoxels, Num_sentences, 2
+			obj_score = self.heat_map(overall_feat)
+			obj_score = F.softmax(obj_score, -1).argmax(-1)
+
+			obj_score = obj_score[supervox[idx:idx+num_p]]
+
+			# Num_points, Num_sentences
+			obj_score = supvox_sen_score[supervox[idx:idx+num_p]].long()
+			# Num_points, Num_sentences
+			gt = batch['ref_lbl'][i].cuda()
+
+			iou = (obj_score * gt).sum(0).float()/((obj_score | gt).sum(0).float() + 1e-5)
+			ious.append(iou.cpu())
+			results.append(obj_score)
+			idx += num_p
+		ious = torch.cat(ious, 0)
+		return results, ious
+
+
---
+++ unet_ref_val.py
@@ -0,0 +1,126 @@
+# Copyright 2016-present, Facebook, Inc.
+ #All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Options
+m = 32 # 16 or 32
+residual_blocks=True #True or False
+block_reps = 2 #Conv block repetition factor: 1 or 2
+num_classes = 20
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import sparseconvnet as scn
+import time
+import os, sys, glob
+import math
+import argparse
+import numpy as np
+from torch.nn.utils.rnn import pack_padded_sequence
+import itertools
+from util import *
+from model import *
+from graph import *
+
+
+use_cuda = torch.cuda.is_available()
+exp_name = 'unet_scale{}_m{}_rep{}_'.format(int(data.scale), int(m), int(block_reps))
+print (exp_name)
+#exp_name='unet_scale50_m32_rep1_ResidualBlocks'
+#save_name = exp_name + '_ins_point'
+#data.batch_size = 4
+
+Voxelize= scn.InputLayer(data.dimension, data.full_scale, mode=4)
+Devoxelize = scn.OutputLayer(data.dimension)
+
+
+def change_lr(optimizer,epoch):
+	p = epoch//50
+	lr = args.lr * (0.1**p)
+	for param_group in optimizer.param_groups:
+		param_group['lr'] = lr
+
+
+DIR = 'supervoxel_ref/'
+
+if not os.path.exists(DIR):
+	os.mkdir(DIR)
+
+bk_name = exp_name + 'bk_1'
+ref_name = exp_name + 'ref_1'
+
+
+backbone_model = SCN_sem()
+ref_model=RefNet_Val()
+if use_cuda:
+	ref_model=ref_model.cuda()
+	backbone_model = backbone_model.cuda()
+
+backbone_model = nn.DataParallel(backbone_model)
+backbone_model.module.linear = nn.Linear(32, 64).cuda()
+checkpoint_restore(backbone_model,bk_name,use_cuda)
+checkpoint_restore(ref_model,ref_name,use_cuda)
+#sem_optimizer = optim.Adam(unet.parameters())
+#training_epoch = 0
+total_ious = []
+total_tp = [0,0]
+total_p = 0
+total_max_iou = []
+for i,batch in enumerate(data.val_data_loader):
+	backbone_model.eval()
+	ref_model.eval()
+	if use_cuda:
+		batch['x'][1]=batch['x'][1].cuda()
+		batch['coords']=batch['coords'].cuda()
+	with torch.no_grad():
+		feats = backbone_model(batch['x'])
+		vox_feats = Voxelize([batch['x'][0], feats])
+		locs = vox_feats.get_spatial_locations()
+		metadata = vox_feats.metadata
+		vox_feats = vox_feats.features
+		supervoxels = []
+		print ('Construct supervoxels...')
+		for i in range(data.batch_size):
+			each_loc = locs[locs[:,-1]==i][:,0:3]
+			each_feat = vox_feats[locs[:,-1]==i]
+			grps = grouping(each_feat.cpu().numpy(), each_loc.cpu().numpy(), 1.0, 10, threshold)
+			grps = torch.from_numpy(grps)
+			grps = grps.unsqueeze(-1) == torch.unique(grps)
+			supervoxels.append(grps.long().argmax(-1).unsqueeze(-1))
+		supervoxels = torch.cat(supervoxels, 0)
+		# Assign supervoxel indexs back to points
+		supervoxels = scn.SparseConvNetTensor(supervoxels.float(), metadata, locs)
+		# Batch_num_points, 
+		supervoxels = Devoxelize(supervoxels)
+
+		supervoxels_ = torch.cat([supervoxels.long(), batch['x'][0][:,-1].unsqueeze(-1)],-1)
+
+		# [(Num_supervoxels, feature_dim), (Num_supervoxels1,feature_dims), ...] len: batch_size
+		supvox_f, _ = gather_supervox(feats, supervoxels_)
+
+		# [(Num_supervoxels, 3), (Num_supervoxels1,3), ...] len: batch_size
+		supvox_coords, _ = gather_supervox(batch['coords'].float(), supervoxels_)
+
+		batch['supvox_f'] = supvox_f
+		batch['supvox_coords'] = supvox_coords
+		batch['supervoxels'] = supervoxels.long().squeeze(-1)
+		print (supervoxels.shape, batch['x'][0].shape)
+		print ('Predict')
+		results, ious = ref_model(batch)
+		print ('iou', ious.mean())
+		total_ious.append(ious)
+		#print ('Saving...')
+		#loc_supvox = torch.cat([batch['coords'].float().cpu(), batch['x'][1].float().cpu(), supervoxels.float()],-1)
+		#for i, name in enumerate(batch['names']):
+		#	print (name)
+		#	np.save(DIR+name+'_ref', results[i].cpu().numpy())
+		#	np.save(DIR+name+'_gt', batch['ref_lbl'][i].cpu().numpy())
+		#	np.save(DIR+name+'_pc', loc_supvox[batch['x'][0][:,-1]==i].numpy())
+		#	np.save(DIR+name+'_sen', batch['sentences'][i])
+total_ious = torch.cat(total_ious, 0)
+print ('Overall IOU')
+print (total_ious.mean())		
